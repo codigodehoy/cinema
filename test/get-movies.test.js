8a204f53b71fc74185ff734ae0da82386948e161
David Cadena <david.cadena@hotmail.com>
2024-02-01 17:10:27 -0500

    feature: Add get_movie_by_id method

--- test/get-movies.test.js
+++ test/get-movies.test.js
@@ -1,22 +1,24 @@
-import { get_movies } from "../javascript/get-movies";
 import { jest } from '@jest/globals';
 
 import data from "./mock/mock-movies";
+import get_movies  from "../javascript/get-movies";
+
 
 describe("Get movie", () => {
-    let resp
+    let response;
 
     beforeAll(() => {
-        resp = {results: data}
-        jest.spyOn(global, "fetch").mockResolvedValue({
-            json: jest.fn().mockResolvedValue(resp)
-        })
+        response = {results: data}
+        const mockFetch = Promise.resolve({
+            json: () => Promise.resolve(response),
+          })
+        global.fetch = jest.fn(() => mockFetch)
     })
 
     it('should retrive successfully data from API ', async () => {
         const movies = await get_movies();
 
-        expect(movies).toEqual(resp.results);
+        expect(movies).toEqual(response.results);
     });
 
     it("should call the fetch function", async () => {
